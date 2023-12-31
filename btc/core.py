83269c0fb2c6982dda15694203598d7569fdde96
mohanson <mohanson@outlook.com>
2023-12-31 13:01:13 +0800

    Implement prikey and pubkey

--- btc/core.py
+++ btc/core.py
@@ -1,6 +1,69 @@
 import btc.ripemd160
 import hashlib
+import json
 
 
 def hash160(data: bytearray) -> bytearray:
     return bytearray(btc.ripemd160.ripemd160(hashlib.sha256(data).digest()))
+
+
+class PriKey:
+    def __init__(self, n: int):
+        self.n = n
+
+    def __repr__(self):
+        return json.dumps(self.json())
+
+    def __eq__(self, other):
+        a = self.n == other.n
+        return a
+
+    def json(self):
+        return f'0x{self.n:064x}'
+
+    def pubkey(self):
+        pubkey = btc.secp256k1.G * btc.secp256k1.Fr(self.n)
+        return PubKey(pubkey.x.x, pubkey.y.x)
+
+
+class PubKey:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return json.dumps(self.json())
+
+    def __eq__(self, other):
+        a = self.x == other.x
+        b = self.y == other.y
+        return a and b
+
+    def json(self):
+        return {
+            'x': f'0x{self.x:064x}',
+            'y': f'0x{self.y:064x}'
+        }
+
+    def sec(self):
+        r = bytearray()
+        if self.y & 1 == 0:
+            r.append(0x02)
+        else:
+            r.append(0x03)
+        r.extend(self.x.to_bytes(32))
+        return r
+
+    @staticmethod
+    def sec_read(data: bytearray):
+        p = data[0]
+        assert p in [0x02, 0x03, 0x04]
+        x = int.from_bytes(data[1:33])
+        if p == 0x04:
+            y = int.from_bytes(data[33:65])
+        else:
+            y_x_y = x * x * x + btc.secp256k1.A.x * x + btc.secp256k1.B.x
+            y = pow(y_x_y, (btc.secp256k1.P + 1) // 4, btc.secp256k1.P)
+            if y & 1 != p - 2:
+                y = -y % btc.secp256k1.P
+        return PubKey(x, y)
--- test/test_core.py
+++ test/test_core.py
@@ -2,6 +2,36 @@ import btc
 
 
 def test_hash160():
-    pubkey = bytearray.fromhex('02fb95541bf75e809625f860758a1bc38ac3c1cf120d899096194b94a5e700e891')
-    pubkey_hash = btc.core.hash160(pubkey)
-    assert pubkey_hash.hex() == '74fe0982ed641b98e94145ee220d2ee3a9693509'
+    pubkey_hash = btc.core.hash160(bytearray([0, 1, 2, 3]))
+    assert pubkey_hash.hex() == '3c3fa3d4adcaf8f52d5b1843975e122548269937'
+
+
+def test_prikey():
+    prikey = btc.core.PriKey(1)
+    pubkey = prikey.pubkey()
+    assert pubkey.x == 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+    assert pubkey.y == 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+
+
+def test_pubkey_sec():
+    pubkey = btc.core.PubKey(
+        0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+        0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+    )
+    assert pubkey.sec().hex() == '0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'
+
+
+def test_pubkey_sec_read():
+    pubkey = btc.core.PubKey.sec_read(bytes.fromhex(''.join([
+        '04',
+        '79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
+        '483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8'
+    ])))
+    assert pubkey.x == 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+    assert pubkey.y == 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+    pubkey = btc.core.PubKey.sec_read(bytes.fromhex(''.join([
+        '02',
+        '79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
+    ])))
+    assert pubkey.x == 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+    assert pubkey.y == 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
