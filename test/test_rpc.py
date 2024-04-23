2647113dbfbe73f47305b5841ef41085a2b14446
mohanson <mohanson@outlook.com>
2024-04-23 19:17:21 +0800

    Add test_generate_to_address

--- test/test_rpc.py
+++ test/test_rpc.py
@@ -1,6 +1,15 @@
 import btc
 
 
+def test_generate_to_address():
+    btc.config.current = btc.config.develop
+    prikey = btc.core.PriKey(1)
+    pubkey = prikey.pubkey()
+    addr = btc.core.address_p2wpkh(pubkey)
+    hash = btc.rpc.generate_to_address(4, addr)
+    assert len(hash) == 4
+
+
 def test_get_block_count():
     btc.config.current = btc.config.develop
     assert btc.rpc.get_block_count() != 0
