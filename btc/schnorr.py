0449e00f11141688d320ad2fc500c114a03377bd
mohanson <mohanson@outlook.com>
2024-05-09 11:05:38 +0800

    2024-05-09 11:05:38

--- btc/schnorr.py
+++ btc/schnorr.py
@@ -1,15 +1,10 @@
+import btc.core
 import btc.secp256k1
-import hashlib
 import random
 import typing
 
 # Schnorr Signatures for secp256k1.
 # See: https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki
-
-def hashtag(name: str, data: bytearray) -> bytearray:
-    tag = bytearray(hashlib.sha256(name.encode()).digest())
-    out = bytearray(hashlib.sha256(tag + tag + data).digest())
-    return out
 
 
 def prikey_implicit(prikey: btc.secp256k1.Fr) -> btc.secp256k1.Fr:
@@ -32,7 +27,7 @@ def sign(prikey: btc.secp256k1.Fr, m: btc.secp256k1.Fr) -> typing.Tuple[btc.secp
     pubkey = btc.secp256k1.G * prikey
     k = prikey_implicit(btc.secp256k1.Fr(random.randint(0, btc.secp256k1.N)))
     r = btc.secp256k1.G * k
-    e_hash = hashtag('challenge', r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
+    e_hash = btc.core.hashtag('challenge', r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
     e = btc.secp256k1.Fr(int.from_bytes(e_hash))
     s = k + e * prikey
     return r, s
@@ -40,6 +35,6 @@ def sign(prikey: btc.secp256k1.Fr, m: btc.secp256k1.Fr) -> typing.Tuple[btc.secp
 
 def verify(pubkey: btc.secp256k1.Pt, m: btc.secp256k1.Fr, r: btc.secp256k1.Pt, s: btc.secp256k1.Fr):
     pubkey = pubkey_implicit(pubkey)
-    e_hash = hashtag('challenge', r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
+    e_hash = btc.core.hashtag('challenge', r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
     e = btc.secp256k1.Fr(int.from_bytes(e_hash))
     return btc.secp256k1.G * s == r + pubkey * e
