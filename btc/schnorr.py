9c7c304290a65fbfa32eb0fd6de8d23635761057
mohanson <mohanson@outlook.com>
2024-05-20 14:55:12 +0800

    Use bytearray

--- btc/schnorr.py
+++ btc/schnorr.py
@@ -33,7 +33,8 @@ def sign(prikey: btc.secp256k1.Fr, m: btc.secp256k1.Fr) -> typing.Tuple[btc.secp
     pubkey = btc.secp256k1.G * prikey
     k = prikey_implicit(btc.secp256k1.Fr(random.randint(0, btc.secp256k1.N)))
     r = btc.secp256k1.G * k
-    e_hash = hash('BIP0340/challenge', r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
+    e_data = bytearray(r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
+    e_hash = hash('BIP0340/challenge', e_data)
     e = btc.secp256k1.Fr(int.from_bytes(e_hash))
     s = k + e * prikey
     return r, s
@@ -41,6 +42,7 @@ def sign(prikey: btc.secp256k1.Fr, m: btc.secp256k1.Fr) -> typing.Tuple[btc.secp
 
 def verify(pubkey: btc.secp256k1.Pt, m: btc.secp256k1.Fr, r: btc.secp256k1.Pt, s: btc.secp256k1.Fr):
     pubkey = pubkey_implicit(pubkey)
-    e_hash = hash('BIP0340/challenge', r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
+    e_data = bytearray(r.x.x.to_bytes(32) + pubkey.x.x.to_bytes(32) + m.x.to_bytes(32))
+    e_hash = hash('BIP0340/challenge', e_data)
     e = btc.secp256k1.Fr(int.from_bytes(e_hash))
     return btc.secp256k1.G * s == r + pubkey * e
